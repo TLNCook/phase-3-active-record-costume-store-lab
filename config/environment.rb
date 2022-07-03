5c330fa62e66e09aa3d7aeefa2e6237dd4065124
Thomas Cook <tlncook85@gmail.com>
2022-07-03 11:21:30 -0600

    commit tree e8bafaa92e628066a347c56c40621d2d3375b748

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,9 @@ ENV["RACK_ENV"] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
 require_all 'app/models'
